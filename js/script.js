1c870eb2214ae98a218a1fc9a67732158a57b526
nely de faria <nelly.nf54@gmail.com>
2021-08-01 14:01:59 -0300

    Criando funções

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Initial data
 let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector('#tela');
 let ctx = screen.getContext('2d');
@@ -19,4 +22,37 @@ function colorClickEvent(e) {
 
     document.querySelector('.color.active').classList.remove('active');
     e.target.classList.add('active');
+}
+
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+    if (canDraw) {
+       draw(e.pageX, e.pageY);
+    }
+}
+
+function mouseUpEvent() {
+    canDraw = false;
+}
+
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
 }
