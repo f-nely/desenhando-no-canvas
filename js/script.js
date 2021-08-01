0855ca7ea2f2dd03922b179ac26a7290c87f7b88
nely de faria <nelly.nf54@gmail.com>
2021-08-01 13:19:38 -0300

    Criando função colorClickEvent e comentando código

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+// Initial data
 let currentColor = 'black';
 
-document.querySelector('.colorArea .color').forEach(item => {
+// Events
+document.querySelectorAll('.colorArea .color').forEach(item => {
     item.addEventListener('click', colorClickEvent);
 });
+
+// Functions
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
